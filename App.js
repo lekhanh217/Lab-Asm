5cbffc0cc76653137c7a748192130fa623bd613b
lekhanh <lekhanh9bth@gmail.com>
2025-03-17 19:13:25 +0700

    add

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { Button, Text, View,TouchableOpacity } from 'react-native';
 import Bai1 from './assets/component/lab5/bai1'
 import Bai2 from './assets/component/lab5/bai2'
 import Bai3 from './assets/component/lab5/bai3'
-////
+//////
 
 function App(){
    return (
